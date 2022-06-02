f22d668be0abe431dabe8fb3a2e6ce307a748faa
Manith De Silva <manith@Maniths-MacBook-Pro.local>
2022-06-02 15:15:14 +0200

    security v1

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 // API KEY
-export const API_KEY = 'AIzaSyD1NclfbZJakRk8AFWGXfevOA5nx0PvBV8'
+export const API_KEY = process.env.REACT_APP_API_KEY
 // CONTEXT KEY
 export const CONTEXT_KEY = '66a3ecd2e8459637c'
